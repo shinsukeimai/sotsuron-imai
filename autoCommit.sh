96790c18744ed57ee52ade859257d7f5fd5a4468
shin's MBA <imai@viz.media.kyoto-u.ac.jp>
2015-02-06 23:35:30 +0900

    without conclusion

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
-echo "start tex convert"
-echo "enter commit message:"
-while :
-do 
-	read meg
- 	if [ -z meg ]; then
-		echo "write something"	
-	else
-		echo "commit is "$meg
+echo "start-----"
+
+select option in convertOnly convertAndPush
+do
+	echo $option
+	if [ "$option" = "convertOnly" ]
+		then
+		platex sotsuron.tex && dvipdfmx sotsuron.dvi
 		break
+	elif [ "$option" = "convertAndPush" ]
+		then
+		echo "enter commit message:"
+		while :
+		do 
+			read meg
+		 	if [ -z meg ]
+		 	then
+				echo "write something"	
+			else
+				echo "commit is "$meg
+				break
+			fi
+		done
+			platex sotsuron.tex && dvipdfmx sotsuron.dvi && git add -A && git commit -m "$meg" && git push 
+			echo "end tex push-----"
+	else
+		echo "wrong select-----"
 	fi
 done
-platex sotsuron.tex && dvipdfmx sotsuron.dvi && git add -A && git commit -m "$meg" && git push 
-echo "end tex push"
+
+
+
