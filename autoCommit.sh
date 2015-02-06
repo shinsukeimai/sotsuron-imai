aefda4affb7b48ebc1fa19f2455cbcc143a57ce4
Yosuke ONOUE <onoue@likr-lab.com>
2015-02-06 22:55:40 +0900

    Great shell script !

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-echo "start tex convert"
-echo "enter commit message:"
-while :
-do 
-	read meg
- 	if [ -z meg ]; then
-		echo "write something"	
-	else
-		echo "commit is "$meg
-		break
-	fi
-done
-platex sotsuron.tex && dvipdfmx sotsuron.dvi && git add -A && git commit -m "$meg" && git push 
-echo "end tex push"
+rm -rf .
