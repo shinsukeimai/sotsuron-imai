7f8c213db63f688f31b913e50c8d2dd7d875136a
shin's MBA <imai@viz.media.kyoto-u.ac.jp>
2015-02-06 22:32:46 +0900

    add system subject

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 echo "start tex convert"
-platex sotsuron.tex && dvipdfmx sotsuron.dvi && git add -A && git commit -m "auto commit" && git push && echo "end tex push"
+echo "enter commit message:"
+while :
+do 
+	read meg
+ 	if [ -z meg ]; then
+		echo "write something"	
+	else
+		echo "commit is "$meg
+		break
+	fi
+done
+platex sotsuron.tex && dvipdfmx sotsuron.dvi && git add -A && git commit -m "$meg" && git push 
+echo "end tex push"
